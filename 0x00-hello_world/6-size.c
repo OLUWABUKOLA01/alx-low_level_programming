a840622e1da008fc7b5e6ce3c489b06fb6e77765
Oluwabukola <adedotunbukola01@gmail.com>
2023-09-07 22:33:54 -0700

    cisfun

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	long long int l;
 	float f;
 
-	printf("Size of char: %lu byte(s)\n", (unsigned long) sizeof(c));
-	printf("Size of int: %lu byte(s)\n", (unsigned long) sizeof(i));
-	printf("Size of long int: %lu byte(s)\n", (unsigned long) sizeof(g));
-	printf("Size of long long int: %lu byte(s)\n", (unsigned long) sizeof(l));
-	printf("Size of float: %lu byte(s)\n", (unsigned long) sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(g));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(l));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(f));
 	return (0);
 }
