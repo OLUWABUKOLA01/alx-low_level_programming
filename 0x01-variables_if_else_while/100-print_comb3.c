8a08f6692d00336b2d5bbfe35a805853c90d5bdb
Oluwabukola <adedotunbukola01@gmail.com>
2023-09-11 11:55:08 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,17 +7,22 @@
 */
 int main(void)
 {
-int s;
-int t;
-for (s = 0; t <= 9; s++)
-{
-	putchar((s % 10) + '0');
-	for (t = s + 1; t <= 9; t++)
-	putchar((t % 10) + '0');
-		continue;
-	putchar(',');
-	putchar(' ');
-}
-putchar('\n');
-return (0);
+	int s;
+	int t;
+
+	for (s = 0; t <= 9; s++)
+	{
+		for (t = s + 1; t <= 9; t++)
+		{
+			putchar((s % 10) + '0');
+			putchar((t % 10) + '0');
+
+			if (s == 8 && t == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
