72904fc9c5101718108d8348693a1d2c2fafe2b0
Oluwabukola <adedotunbukola01@gmail.com>
2023-09-11 09:17:45 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: Print all possible different combinations of two digits
+ * Return: 0
+*/
+int main(void)
+{
+int s;
+int t;
+for (s = 0; t <= 9; s++)
+{
+	putchar((s % 10) + '0');
+	for (t = s + 1; t <= 9; t++)
+		continue;
+	putchar(',');
+	putchar(' ');
+}
+putchar('\n');
+return (0);
+}
