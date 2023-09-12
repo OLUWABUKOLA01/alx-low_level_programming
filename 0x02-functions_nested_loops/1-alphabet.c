fd7464015beb005955dc877bf831b788578ed1f2
Oluwabukola <adedotunbukola01@gmail.com>
2023-09-12 16:02:41 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - Print alphabet
+ * Description: Print the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	{
+		for (i = 'a'; i <= 'z'; i++)
+
+		_putchar(i);
+
+	}
+	_putchar('\n');
+}
