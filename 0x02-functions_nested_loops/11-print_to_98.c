697cb28875bcb342ea02d5134d01f792188d94c6
Oluwabukola <adedotunbukola01@gmail.com>
2023-09-13 12:23:43 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,7 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (;n <= 98; n++)
+		for (; n <= 98; n++)
 	{
 		if (n == 98)
 	{
@@ -29,4 +29,20 @@ void print_to_98(int n)
 	}
 	}
 
+	else
+	{
+		for (; n >= 98; n--)
+	{
+		if (n == 98)
+	{
+		printf("%d", n);
+		printf("\n");
+		break;
+	}
+	else
+	{
+		printf("%d,", n);
+	}
+	}
+	}
 }
