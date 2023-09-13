42969b9af5e47315d4e12a478a182fda662de6cc
Oluwabukola <adedotunbukola01@gmail.com>
2023-09-12 17:09:18 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,13 @@ void print_alphabet_x10(void)
 	char i = 'a';
 	int j = 1;
 
-	while (1 < 10)
+	while (j <= 10)
 	{
 		for (i = 'a'; i <= 'z'; i++)
 
 		_putchar(i);
 		j++;
-
 		_putchar('\n');
+
 	}
 }
