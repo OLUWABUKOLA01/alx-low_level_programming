b0fcd72431b9d8760f5715d7b0c61363dcdf126f
Oluwabukola <adedotunbukola01@gmail.com>
2023-09-14 15:43:51 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,25 +2,24 @@
 
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14
- *
- *
+ * @j: first line
+ * @k: second line
  */
 
 void more_numbers(void)
 {
 	int j, k;
 
-	for (j = 1; j <= 10; j++)
+	for (j = 0; j < 10; j++)
 
 	{
 
 	for (k = 0; k <= 14; k++)
 	{
 
-		if (j > 10)
-		_putchar(1);
-
-		_putchar((j % 10 + '0');
+		if (k > 10)
+		_putchar(k / 10 + '0');
+		_putchar(k % 10 + '0');
 
 	}
 	_putchar('\n');
