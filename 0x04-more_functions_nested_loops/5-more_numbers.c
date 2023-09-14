4e52fb5c838db730817394a7435e259fbf87794e
Oluwabukola <adedotunbukola01@gmail.com>
2023-09-14 15:02:00 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ *
+ *
+ */
+
+void more_numbers(void)
+{
+	int j, k;
+
+	for (j = 1; j <= 10; j++)
+
+	{
+
+	for (k = 0; k <= 14; k++)
+	{
+
+		if (j > 10)
+		_putchar(1);
+
+		_putchar((j % 10 + '0');
+
+	}
+	_putchar('\n');
+
+	}
+
+}
