13938c98b3350f94e0f1cad3428bab05b884e6cf
Oluwabukola <adedotunbukola01@gmail.com>
2023-09-14 16:10:18 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,15 +10,15 @@ void more_numbers(void)
 {
 	int j, k;
 
-	for (j = 0; j < 10; j++)
+	for (j = 1; j <= 10; j++)
 
 	{
 
 	for (k = 0; k <= 14; k++)
 	{
 
-		if (k > 10)
-		_putchar(k / 10 + '0');
+		if (k >= 10)
+		_putchar('1');
 		_putchar(k % 10 + '0');
 
 	}
