18bab88b86d9be4d55e64397ebbc11a4f3ec6f58
Oluwabukola <adedotunbukola01@gmail.com>
2023-09-14 17:52:24 -0700

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - print a line in the terminal
+ *
+ * @n: number of times the character _ should be printed
+ */
+
+void print_line(int n)
+{
+	int t;
+
+	if (n > 0)
+	{
+		for (t = 1; t <= n; t++)
+		{
+			_putchar('_');
+		}
+	_putchar('\n');
+	}
+}
+
