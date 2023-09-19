266b7ea4c7e872d5d72f0ea98862be55f25c08a8
Oluwabukola <adedotunbukola01@gmail.com>
2023-09-19 08:30:53 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: array
+ * @n: element of an array of integers
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	printf("%d", a[i]);
+	{
+		if (i < n - 1)
+			printf(", ");
+	}
+	printf("\n");
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN
 #define MAIN
+#include <stdio.h>
 
 int _putchar(char c);
 
