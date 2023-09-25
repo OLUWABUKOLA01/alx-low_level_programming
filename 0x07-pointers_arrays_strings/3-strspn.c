a8bcb897a5ef24bcc4ae0116ed3fe69c772441b3
Oluwabukola <adedotunbukola01@gmail.com>
2023-09-25 15:23:46 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strspn -  length of a prefix substring
+ * @s: string
+ * @accept: bytes
+ *
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int x;
+	int y;
+
+	while (*s)
+	{
+		for (y = 0; accept[y]; y++)
+		{
+
+			if (*s == accept[y])
+			{
+				x++;
+				break;
+			}
+
+			else if (accept[y + 1] == '\0')
+				return (x);
+		}
+		s++;
+	}
+	return (x);
+}
