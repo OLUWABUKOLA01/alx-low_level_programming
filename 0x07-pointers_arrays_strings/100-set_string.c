dbd0ac7aae26ab036108ca7e22665eb5ea053c74
Oluwabukola <adedotunbukola01@gmail.com>
2023-09-26 07:49:20 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string -  set the value of a pointer to a char
+ * @s: first function
+ * @to: second function
+ * Return: 0 (Success)
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
