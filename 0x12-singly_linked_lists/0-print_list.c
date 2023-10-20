952397f83a2095a8d7eeb700e0b4ec1993c07729
Oluwabukola <adedotunbukola01@gmail.com>
2023-10-19 18:26:11 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: Definition for linked list 
+ *
+ * Return: Linked list
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int node = 0;
+
+	while (h != 0)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+
+		node++;
+	}
+	return (node);
+}
