2aebd56f7a6483117817391026c1a1e6f09cba0e
Oluwabukola <adedotunbukola01@gmail.com>
2023-10-19 18:40:23 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len - function to list length of string
+ *
+ * @h: Const
+ *
+ * Return: Length of string
+ */
+
+size_t list_len(const list_t *h)
+{
+	unsigned int a = 0;
+
+	while (h == 0)
+	
+		return (a);
+}
