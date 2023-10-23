028da94eb5f42ee99d86c058e6522de8efa5e532
Oluwabukola <adedotunbukola01@gmail.com>
2023-10-23 09:23:48 -0700

    add all the elements of a listint_t list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ *  print_listint - prints all the elements of a listint_t list
+ * @h: pointer to a list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	for (nodes = 0; h; nodes++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (nodes);
+}
