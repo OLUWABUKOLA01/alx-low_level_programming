7fc564b5519f2fe39af3169d09bb73cb85f31b13
“Oluwabukola <adedotunbukola01@gmail.com>
2023-12-07 05:43:12 -0800

    Print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: pointer to  dlistint_t
+ *
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	if (!h)
+		return (0);
+
+	while (h->prev)
+		h = h->prev;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
